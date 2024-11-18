345b13d3e201a64f6709224b021f863a48eb4bd4
Anderton <23054103@myrp.edu.sg>
2024-11-18 14:52:22 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+function Input() {
+  const [userName, setUserName] = useState("");
+  const [studentID, setStudentID] = useState("");
+  const [userAge, setUserAge] = useState("");
+  const [lunchOption, setLunchOption] = useState("");
+
+
+  const handleNameInput = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const hanldStudentIDInput = (e) => {
+    setStudentID(e.target.value);
+  }
+
+  const handleAgeInput = (e) => {
+    setUserAge(e.target.value);
+  }
+
+  return (
+    <>
+      <div>
+        <form>
+          <label for="name">Name: </label>
+          <input value={userName} id="name" type='text' onChange={handleNameInput} />
+          <br />
+          <label for="studentID">Student ID: </label>
+          <input value={studentID} id="studentid" type='text' onChange={hanldStudentIDInput} />
+          <br />
+          <label for="age">Age: </label>
+          <input value={userAge} id="age" type='number' onChange={handleAgeInput} />
+        </form>
+        <br/>
+        <div>
+          <label>Lunch Option:</label>
+          <br />
+          <input
+            type="radio"
+            id="vegetarian"
+            name="lunch"
+            value="Vegetarian"
+            onChange={(e) => setLunchOption(e.target.value)}
+          />
+          <label htmlFor="vegetarian">Vegetarian</label>
+          <br />
+          <input
+            type="radio"
+            id="nonVegetarian"
+            name="lunch"
+            value="Non-Vegetarian"
+            onChange={(e) => setLunchOption(e.target.value)}
+          />
+          <label htmlFor="nonVegetarian">Non-Vegetarian</label>
+          <br />
+          <input
+            type="radio"
+            id="vegan"
+            name="lunch"
+            value="Vegan"
+            onChange={(e) => setLunchOption(e.target.value)}
+          />
+          <label htmlFor="vegan">Vegan</label>
+        </div>
+
+        <p>Name: {userName} <br/>
+        StudentID: {studentID} <br/>
+        Age: {userAge} <br/>
+        Lunch Option: {lunchOption}</p>
+      </div>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Input />
   );
 }
 
